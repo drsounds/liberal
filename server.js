d731b819b6f7a0bd2677a8fb7149ec7bcb9a0999
Alexander Forselius <drsounds@gmail.com>
2017-05-28 11:33:39 +0200

    Fixed routing error

--- server.js
+++ server.js
@@ -9,21 +9,39 @@ var bodyParser = require('body-parser');
 
 
 
+app.use(express.static(__dirname + '/client/'));
+app.get('/*', function (req, res) {
+    var protocol = req.connection.encrypted ? 'https' : 'http';
+    if (req.host.indexOf('liberal-drsounds.c9users.io') != -1) {
+        protocol = 'https';
+    }
+    var index = fs.readFileSync(__dirname + '/client/index.html', 'utf8');
+
+    index = index.replace('https://liberal-drsounds.c9users.io',  protocol + '://' + req.host + ':' + (process.env.PORT || 9261) + '');
+    console.log(index);
+    res.write(index);
+    res.end();
+});
 app.use(cookieSession({
     name: 'session',
     keys: ['key1', 'key2']
 }));
 
-app.use(express.static(__dirname + '/client/'));
 app.use('/api', api.server);
 app.get('/callback.html', function (req, res) {
     var index = fs.readFileSync(__dirname + '/client/callback.html');
     res.write(index);
     res.end();
 });
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
+    var protocol = req.connection.encrypted ? 'https' : 'http';
+    if (req.host.indexOf('liberal-drsounds.c9users.io') != -1) {
+        protocol = 'https';
+    }
+    var index = fs.readFileSync(__dirname + '/client/index.html', 'utf8');
 
-    var index = fs.readFileSync(__dirname + '/client/index.html');
+    index = index.replace('https://liberal-drsounds.c9users.io',  protocol + '://' + req.host + ':' + (process.env.PORT || 9261) + '');
+    console.log(index);
     res.write(index);
     res.end();
 });
