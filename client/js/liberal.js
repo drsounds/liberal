2a05055f25c599fc3eacf91c66c9fc84a95594d8
Dr. Sounds <management@dr-sounds.com>
2017-05-29 22:02:47 +0000

    Polished Obama skin

--- client/js/liberal.js
+++ client/js/liberal.js
@@ -320,11 +320,19 @@ document.registerElement('sp-themeeditor', SPThemeEditorElement);
 class SPAppHeaderElement extends HTMLElement {
     attachedCallback() {
         if (!this.created) {
-        if (!this.searchForm) {
-            this.searchForm = document.createElement('sp-searchform');
-            this.appendChild(this.searchForm);
-        }
-        this.created = true;
+            this.innerHTML = '<button id="btnBack" class="fa fa-arrow-left" onclick="history.back()"><button class="fa fa-arrow-right" onclick="history.forward()"></button><div style="flex: 5"></div>';
+            if (!this.searchForm) {
+                this.searchForm = document.createElement('sp-searchform');
+                this.appendChild(this.searchForm);
+                this.searchForm.style.marginRight = '5pt';
+            }
+            this.loginButton = document.createElement('button');
+            this.loginButton.innerHTML = 'Log in';
+            this.loginButton.addEventListener('click', (e) => {
+               GlobalViewStack.navigate('bungalow:internal:login'); 
+            });
+            this.appendChild(this.loginButton);
+            this.created = true;
         }
     }
     
